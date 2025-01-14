407ced4c4db08e2a5196eb9dfe1aa5c3b6d1453b
Himanshu <himanshusingh8102004@gmail.com>
2025-01-14 09:50:53 +0000

    datatypes

---
+++ js01/02_datatypes.js
@@ -0,0 +1,35 @@
+"use strict"; //for newer version of js.
+
+//alert( 3+3 ); //we are using nodejs not browser
+
+
+let number= 44
+let name = "himanshu"
+let loggedIn = true
+let place;
+let age = null
+//datatypes
+
+//number ==> 2 to the power 53
+//string ==>
+//bigint 
+//boolean
+//null ==> also a standalone value
+//undefined 
+
+console.log(3 + 4);
+
+console.log(name);
+
+console.log(typeof loggedIn );
+
+console.log(typeof place);
+
+console.log(typeof age);
+
+
+
+
+
+
+
